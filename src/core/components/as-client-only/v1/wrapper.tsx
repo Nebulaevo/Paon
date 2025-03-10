aaaf409c9fe257e3fe3a54ed89ddd8b51d7f108e
Nebulaevo <didier.martin.dev@gmail.com>
2025-03-10 21:01:12 +0100

    patched "asClientOnly" so that it wouldn't cause hydration errors (by forcing second re-render)

--- src/core/components/as-client-only/v1/wrapper.tsx
+++ src/core/components/as-client-only/v1/wrapper.tsx
@@ -1,11 +1,15 @@
-import type { ComponentType, FunctionComponent } from 'react'
-
-import { isExecutedOnClient } from '@core:utils/execution-context/execution-context'
-
+import { useEffect, useState, type ComponentType, type FunctionComponent } from 'react'
 
 function asClientOnly<P extends object>( Component:ComponentType<P> ): FunctionComponent<P> {
     return function ClientComponent( props:P ) {
-        return isExecutedOnClient() ? <Component {... props} /> : <></>
+
+        // dirty but we do not trigger a lot of re-renders
+        // and it allows to avoid hydration errors
+        // (otherwise rendered app is different between server and client)
+        const [mounted, setMounted] = useState<Boolean>(false)
+        useEffect(()=> setMounted(true))
+
+        return mounted ? <Component {... props} /> : null
     }
 }
 
