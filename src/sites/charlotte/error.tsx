5389ae9320aed16d1799ab7bc2575d1c3c47af57
Nebulaevo <didier.martin.dev@gmail.com>
2025-03-19 22:41:15 +0100

    cleaning

--- src/sites/charlotte/error.tsx
+++ src/sites/charlotte/error.tsx
@@ -1,9 +1,3 @@
 import Error from "@core:components/error-component-default/v1/component"
 
-// function Error() {
-//     return <>
-//         <p>Oops.. Something got wrong</p>
-//     </>
-// }
-
 export default Error
