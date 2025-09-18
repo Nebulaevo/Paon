4fdf0a787c0a0ecff202611f07e655440d72ba48
Nebulaevo <didier.martin.dev@gmail.com>
2025-09-18 21:27:28 +0200

    created util generating ids

---
+++ src/core/utils/id/v1/utils.ts
@@ -0,0 +1,54 @@
+
+/** Generates non-secure simple random ids */
+function getSimpleRandomId(): string {
+    return Math.random().toString(36).slice(2)
+}
+
+/** Returns an object allowing to generate unique ids
+ * 
+ * @returns key/value object {get: func, unregister: func} :
+ * - `get()` : returns a unique id
+ * - `unregister(id: string)` : removes a given id from the existing id set.
+ */
+function getUniqueIdGenerator() {
+    
+    const EXISTING_IDS = new Set<string>()
+    
+    /** Returns a unique ID
+     * 
+     * (by storing generated ids in a set)
+     * 
+     * @throws Error if while loop generating ids fails to create a unique id 20 times
+     */
+    function get(): string {
+        let id;
+        let iter = 0
+        do {
+            id = getSimpleRandomId() + '-' + getSimpleRandomId()
+            iter ++
+
+            if (iter > 20) throw new Error(
+                'ID generation failed to generate a unique id 20 times in a row, '
+                + 'while loop was forcfully closed'
+            )
+        } while (EXISTING_IDS.has(id))
+    
+        EXISTING_IDS.add(id)
+        return id
+    }
+
+    /** Removes an id from the existing ids set
+     * 
+     * (allowing it to be generated again)
+     */
+    function unregister(id: string) {
+        EXISTING_IDS.delete(id)
+    }
+
+    return {
+        get, 
+        unregister
+    }
+}
+
+export { getSimpleRandomId, getUniqueIdGenerator }
